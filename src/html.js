390a3e9fecc14d8006875eb1882c635bea8b8a68
frankyoh <frankyoh@tmon.co.kr>
2019-11-03 23:19:24 +0900

    remove redirect from index.html

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
           />
-          <meta httpEquiv="Refresh" content="0; url=https://blog.advenoh.pe.kr" />
           <meta name="google-site-verification" content="zJ5_7xeoUF73cEE-Vkt2yFp28N2Az0r4-r-zBWnWGQI" />
           {this.props.headComponents}
         </head>
