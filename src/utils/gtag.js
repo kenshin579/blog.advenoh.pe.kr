2ff6aefc2f44950faf264601ca9b178ea508e3a4
kenshin579 <kenshin579@hotmail.com>
2020-11-19 16:04:03 +0900

    [BLOG-165] google analytics gtag script 추가

---
+++ src/utils/gtag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-B0KLVK60W1');
