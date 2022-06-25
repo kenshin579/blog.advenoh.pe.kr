10edce63596314fb88e45f0a262c51550c78b71e
kenshin579 <kenshin579@hotmail.com>
2022-06-25 15:32:00 +0900

    [#324] 예전 theme 파일 삭제

--- SiteConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteTitle: 'Advenoh blog', // Site title.
-  siteTitleShort: 'Advenoh blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: 'Advenoh blog', // Alternative site title for SEO.
-  siteUrl: 'https://blog.advenoh.pe.kr', // Domain of your website without pathPrefix.
-  siteDescription: 'Advenoh IT Blog web site.', // Website description used for RSS feeds/meta description tag.
-};
